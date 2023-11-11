7a73c7cdb3371e34d94d16e89d2cf7a71512c8a3
Patricia <patricia.femenias@gmail.com>
2023-11-11 13:29:05 +0100

    Add Header component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import AppStyled from "./AppStyled";
+import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
   return (
     <AppStyled>
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/monsters" />} />
---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = (): React.ReactElement => {
+  return (
+    <img
+      className="header-logo"
+      src="/images/logo.webp"
+      alt="Monster Squad logo"
+      width="820"
+      height="400"
+    ></img>
+  );
+};
+export default Header;
