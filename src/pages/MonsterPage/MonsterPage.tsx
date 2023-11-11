41a880204659a38795eb1cc8487c3319c9f851b1
Patricia <patricia.femenias@gmail.com>
2023-11-11 16:31:18 +0100

    Add MonstersPage

---
+++ src/pages/MonsterPage/MonsterPage.tsx
@@ -0,0 +1,9 @@
+const MonsterPage = (): React.ReactElement => {
+  return (
+    <section>
+      <h2 className="title-page">💀Monsters friends and enemies💀</h2>
+    </section>
+  );
+};
+
+export default MonsterPage;
