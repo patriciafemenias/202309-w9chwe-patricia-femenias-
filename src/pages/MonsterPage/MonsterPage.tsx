374a2f83ec9f9b784290d0569a71c73cc97dbfac
Patricia <patricia.femenias@gmail.com>
2023-11-11 16:32:16 +0100

    Add styles to heading's pages

--- src/pages/MonsterPage/MonsterPage.tsx
+++
@@ -1,9 +0,0 @@
-const MonsterPage = (): React.ReactElement => {
-  return (
-    <section>
-      <h2 className="title-page">💀Monsters friends and enemies💀</h2>
-    </section>
-  );
-};
-
-export default MonsterPage;
