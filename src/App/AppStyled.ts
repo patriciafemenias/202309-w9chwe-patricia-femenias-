4425b8ea97163aa525f8e7f5aef8a50da95f4470
Patricia <patricia.femenias@gmail.com>
2023-11-11 12:46:21 +0100

    Add AppStyled

---
+++ src/App/AppStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  align-items: center;
+`;
+
+export default AppStyled;
