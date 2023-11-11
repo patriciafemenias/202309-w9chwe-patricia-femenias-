a2305c104a7fb6bc7ae5730df3c54f1950025278
Patricia <patricia.femenias@gmail.com>
2023-11-11 23:38:18 +0100

    Fix MonsterCard styles

--- src/components/MonsterCard/MonsterCard.tsx
+++ src/components/MonsterCard/MonsterCard.tsx
@@ -20,11 +20,11 @@ const MonsterCard = ({
         />
         <h2 className="monster-card__name">{name}</h2>
         <dl className="monster-card__info-container">
-          <dt className="monster-card__title-info">Type of monster:</dt>
+          <dt className="monster-card__title-info">♥ Type of monster ♥</dt>
           <dd className="monster-card__info">{monsterType}</dd>
-          <dt className="monster-card__title-info">Age:</dt>
+          <dt className="monster-card__title-info">♥ Age ♥</dt>
           <dd className="monster-card__info">{age} years old</dd>
-          <dt className="monster-card__title-info">Relationship:</dt>
+          <dt className="monster-card__title-info">♥ Relationship ♥</dt>
           <dd className="monster-card__info">{isFriend}</dd>
         </dl>
       </div>
--- src/components/MonsterCard/MonsterCardStyled.ts
+++ src/components/MonsterCard/MonsterCardStyled.ts
@@ -14,32 +14,40 @@ const MonsterCardStyled = styled.article`
   padding: 20px;
   padding-top: 40px;
   box-shadow: 0 0 25px ${({ theme }) => theme.colors.accent};
+  font-size: 16px;
 
   .monster-card {
     &__data {
-      display: flex;
       flex-direction: column;
       align-items: center;
       margin: 0;
       color: ${({ theme }) => theme.colors.light};
     }
 
-    &__title {
-      color: ${({ theme }) => theme.colors.light};
+    &__name {
+      color: ${({ theme }) => theme.colors.accent};
       text-transform: uppercase;
       margin: 20px;
+      font-size: 20px;
     }
 
     &__info-container {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      grid-gap: 10px;
-      align-items: center;
+      display: flex;
+      flex-direction: column;
+      grid-gap: 5px;
+      align-items: flex-start;
       justify-items: start;
+      justify-content: flex-start;
     }
 
     &__title-info {
       font-weight: bold;
+      text-transform: uppercase;
+      color: #76bcce;
+    }
+
+    &__info {
+      color: #dcf967;
     }
 
     &__image {
